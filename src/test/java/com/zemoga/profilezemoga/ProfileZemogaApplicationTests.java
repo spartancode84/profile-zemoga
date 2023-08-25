13e32dcedf0444752e67b32593274ed30c72ad03
Oscar Alfredo Chafloque Tampeck <oscar.chafloque@soytul.com>
2023-08-24 23:59:23 -0500

    proyecto de consolidación

---
+++ src/test/java/com/zemoga/profilezemoga/ProfileZemogaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zemoga.profilezemoga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileZemogaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
